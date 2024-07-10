bf4cba4902f98231f7229456a2ae7b44ccabeaf2
raphaelle <raflanelle@hotmail.com>
2024-07-10 16:28:20 +0200

    mymoviz avec vercel

--- components/Home.js
+++ components/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
 
   useEffect(() => {
-    fetch('http://localhost:3000/movies')
+    fetch('https://mymoviz-backend-puce-alpha.vercel.app/movies')
       .then(response => response.json())
       .then(data => {
         let cards = [];
